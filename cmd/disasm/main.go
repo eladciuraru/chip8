6aced4bbf4d6e60baa5252c7ea13abfbb2120348
eladciuraru <eladciuraru@gmail.com>
2020-03-04 01:03:22 +0200

    Changed indentation from tabs to spaces

--- cmd/disasm/main.go
+++ cmd/disasm/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"github.com/eladciuraru/chip8/pkg/disasm"
+    "fmt"
+    "github.com/eladciuraru/chip8/pkg/disasm"
 )
 
 
 func main() {
-	dis, err := disasm.FromFile("roms/maze.bin")
-	if err != nil {
-		panic(err)
-	}
+    dis, err := disasm.FromFile("roms/maze.bin")
+    if err != nil {
+        panic(err)
+    }
 
-	fmt.Printf("%#v\n", dis)
-	fmt.Println(dis.InstAt(0))
-	fmt.Println(dis.InstAt(2))
-	fmt.Println(dis.InstAt(3))
+    fmt.Printf("%#v\n", dis)
+    fmt.Println(dis.InstAt(0))
+    fmt.Println(dis.InstAt(2))
+    fmt.Println(dis.InstAt(3))
 
-	for iter := dis.Iterator(); iter.Next(); {
-		fmt.Println(iter.Value())
-	}
+    for iter := dis.Iterator(); iter.Next(); {
+        fmt.Println(iter.Value())
+    }
 }
--- pkg/disasm/disasm.go
+++ pkg/disasm/disasm.go
@@ -1,10 +1,10 @@
 package disasm
 
 import (
-	"io/ioutil"
-	"os"
-	"fmt"
-	"io"
+    "io/ioutil"
+    "os"
+    "fmt"
+    "io"
 )
 
 
@@ -12,49 +12,49 @@ const InstructionSize uint = 0x02
 
 
 type Disasm struct {
-	romData []byte
+    romData []byte
 }
 
 
 func New(rom []byte) *Disasm {
-	return &Disasm{
-		romData: rom,
-	}
+    return &Disasm{
+        romData: rom,
+    }
 }
 
 
 func FromReader(reader io.Reader) (*Disasm, error) {
-	romData, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read all: %w", err)
-	}
+    romData, err := ioutil.ReadAll(reader)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read all: %w", err)
+    }
 
-	return New(romData), nil
+    return New(romData), nil
 }
 
 
 func FromFile(filename string) (*Disasm, error) {
-	// Maybe it is better to just use ioutil.ReadFile instead of reusing
-	// `FromReader` function
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open %v: %w", filename, err)
-	}
-	defer file.Close()
+    // Maybe it is better to just use ioutil.ReadFile instead of reusing
+    // `FromReader` function
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open %v: %w", filename, err)
+    }
+    defer file.Close()
 
-	return FromReader(file)
+    return FromReader(file)
 }
 
 
 func (dis *Disasm) InstAt(index uint) (string, error) {
-	if index < 0 || index >= uint(len(dis.romData) - 1) {
-		return "", fmt.Errorf("index %d out of rom range", index)
-	}
+    if index < 0 || index >= uint(len(dis.romData) - 1) {
+        return "", fmt.Errorf("index %d out of rom range", index)
+    }
 
-	opcode := uint16(dis.romData[index]) << 8 +
-			  uint16(dis.romData[index + 1])
+    opcode := uint16(dis.romData[index]) << 8 +
+              uint16(dis.romData[index + 1])
 
-	return fmt.Sprintf("%#04x", opcode), nil
+    return fmt.Sprintf("%#04x", opcode), nil
 }
 
 
@@ -62,32 +62,32 @@ func (dis *Disasm) InstAt(index uint) (string, error) {
 // go routine with a channel to support `range` keyword
 // since it can leak a go routine in case of early exit from iter loop
 func (dis *Disasm) Iterator() iterator {
-	return iterator{
-		dis:   dis,
+    return iterator{
+        dis:   dis,
 
-		// Since we use uint and can't cast -InstructionSize into uint,
-		// so we implement the two's complement
-		index: ^InstructionSize + 1,
-	}
+        // Since we use uint and can't cast -InstructionSize into uint,
+        // so we implement the two's complement
+        index: ^InstructionSize + 1,
+    }
 }
 
 
 type iterator struct {
-	dis   *Disasm
-	index uint
+    dis   *Disasm
+    index uint
 }
 
 
 func (iter *iterator) Next() bool {
-	iter.index += InstructionSize
+    iter.index += InstructionSize
 
-	return iter.index < uint(len(iter.dis.romData) - 1)
+    return iter.index < uint(len(iter.dis.romData) - 1)
 }
 
 
 func (iter *iterator) Value() string {
-	// No need to check for error since we have a check at 'Next' function
-	inst, _  := iter.dis.InstAt(iter.index)
+    // No need to check for error since we have a check at 'Next' function
+    inst, _  := iter.dis.InstAt(iter.index)
 
-	return inst
+    return inst
 }
